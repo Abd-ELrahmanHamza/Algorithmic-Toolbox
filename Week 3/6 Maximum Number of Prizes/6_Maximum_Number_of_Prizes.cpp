42b338a223a0b15b29151a4dd6abbc52d357102b
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2021-07-22 20:52:47 +0200

    Maximum Number of Prizes

--- Week 3/6 Maximum Number of Prizes/6_Maximum_Number_of_Prizes.cpp
+++ Week 3/6 Maximum Number of Prizes/6_Maximum_Number_of_Prizes.cpp
@@ -23,12 +23,9 @@ int main()
         v.push_back(i);
         i++;
     }
-    //cout << v[i - 3] << endl;
     if (sum > n)
     {
         v.pop_back();
-        //cout << (n ) << endl;
-        //cout << sum<<" "<<i << endl;
         v[i - 3] += (n - (sum - (i - 1)));
     }
     cout << v.size() << endl;
@@ -40,38 +37,3 @@ int main()
     }
     return 0;
 }
-
-// #include <iostream>
-// #include <vector>
-// using namespace std;
-// int main()
-// {
-//     long long sum;
-//     cin >> sum;
-//     int i = 1;
-//     vector<int> v;
-//     long long n = sum;
-//     while (sum > 0)
-//     {
-//         if (sum - i >= 0)
-//         {
-//             v.push_back(i);
-//             sum -= i;
-//         }
-//         else
-//             break;
-//         i++;
-//     }
-//     if (n != sum)
-//         v.push_back((n - sum) * 2);
-//     for (int i = 0; i < v.size(); i++)
-//     {
-//         if (v[i] != (n - sum) )
-//         {
-//             cout << v[i];
-//             if (i != v.size() - 1)
-//                 cout << " ";
-//         }
-//     }
-//     return 0;
-// }
