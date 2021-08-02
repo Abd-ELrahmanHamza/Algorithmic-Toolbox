1525b5c149d13d8cdcc67102dc1911f79fd31995
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2021-08-02 03:18:59 +0200

    Quicksort3

--- Week 4/QuickSort.cpp
+++ Week 4/QuickSort.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int partition(vector<int>& A, int l, int r)
+pair<int, int> partition3(vector<int> &A, int l, int r)
 {
     int j = l;
     for (int i = l + 1; i <= r; i++)
@@ -12,21 +12,31 @@ int partition(vector<int>& A, int l, int r)
             swap(A[i], A[j]);
         }
     }
+    int m2 = j;
+    j = l;
+    for (int i = l + 1; i <= m2; i++)
+    {
+        if (A[i] < A[l])
+        {
+            j++;
+            swap(A[i], A[j]);
+        }
+    }
     swap(A[j], A[l]);
-    return j;
+    return make_pair(j, m2);
 }
-void RandomizedQuickSort(vector<int>& A, int l, int r)
+void RandomizedQuickSort(vector<int> &A, int l, int r)
 {
     if (l >= r)
         return;
-    swap(A[l],A[l+rand()%r]);
-    int m = partition(A, l, r);
-    RandomizedQuickSort(A, l, m - 1);
-    RandomizedQuickSort(A, m + 1, r);
+    swap(A[l], A[l + (rand() % (r-l))]);
+    pair<int, int> m = partition3(A, l, r);
+    RandomizedQuickSort(A, l, m.first - 1);
+    RandomizedQuickSort(A, m.second + 1, r);
 }
 int main()
 {
-    vector<int> v = {1, 5, 7, 2, 34, 9, 6};
+    vector<int> v = {1, 5, 7,34,34, 2, 34, 9, 6, 6, 6, 1, 1, 2, 2, 2};
     RandomizedQuickSort(v, 0, v.size() - 1);
     for (int x : v)
         cout << x << " ";
